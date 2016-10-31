989f2f53807740a6c85cdd2026833b59835da45b
Michael <michael.j.feneley@gmail.com>
2016-10-31 08:15:58 -0400

    First pass

--- src/jre-auditor.py
+++ src/jre-auditor.py
@@ -25,6 +25,16 @@ class JREAuditor:
         self.has_deployment_file()
         self.has_properties_file()
 
+        """
+        self.permission_dialog_disabled()
+        self.permission_dialog_locked()
+        self.publisher_revocation_enabled()
+        self.publisher_revocation_locked()
+        self.certificate_validation_enabled()
+        self.certificate_validation_locked()
+        self.config_keys_set()
+        """
+
     def get_deployment_path(self):
         holder = open(HOLDER_FILE, 'w')
         call(["find", "/usr", "-name", DEPLOYMENT_FILENAME], stdout=holder)
@@ -34,16 +44,20 @@ class JREAuditor:
 
         if(os.path.getsize(HOLDER_DIR) == 0):
             self.deployment_path = None
+            call(["rm", HOLDER_FILE])
             return 0
 
         for file in holder:
             if(line != ''):
                 self.deployment_path = line
                 holder.close()
+                call(["rm", HOLDER_FILE])
+
                 return 1
             else:
                 self.deployment_path = None
                 holder.close()
+                call(["rm", HOLDER_FILE])
                 return 0
 
     def get_properties_path(self):
@@ -54,16 +68,19 @@ class JREAuditor:
 
         if(os.path.getsize(HOLDER_DIR) == 0):
             self.deployment_path = None
+            call(["rm", HOLDER_FILE])
             return 0
 
         for file in holder:
             if(line != ''):
                 self.deployment_path = line
                 holder.close()
+                call(["rm", HOLDER_FILE])
                 return 1
             else:
                 self.deployment_path = None
                 holder.close()
+                call(["rm", HOLDER_FILE])
                 return 0
 
     def has_deployment_file(self):
@@ -91,6 +108,22 @@ class JREAuditor:
             return True
 
 
+    def permission_dialog_disabled(self):
+        """Check SV-43596r1_rule: The dialog to enable users to grant 
+        permissions to execute signed content from an un-trusted 
+        authority must be disabled.
+
+        Finding ID: V-32828     
+        """
+        config = open(self.properties_path, 'r')
+
+        locked = False
+        for line in config_file:
+            if 'deployment.security.askgrantdialog.notinca=false' in line:
+                locked = True
+        config.close()
+        return locked
+
 
     def permission_dialog_locked(self):
         """Check SV-43601r1_rule: The dialog to enable users to grant 
@@ -105,27 +138,8 @@ class JREAuditor:
         for line in config_file:
             if line == 'deployment.security.askgrantdialog.notinca':
                 locked = True
-        return locked
-
-
-
-    def permission_dialog_disabeld(self):
-        """Check SV-43596r1_rule: The dialog to enable users to grant 
-        permissions to execute signed content from an un-trusted 
-        authority must be disabled.
-
-        Finding ID: V-32828     
-        """
-        config = open(self.properties_path, 'r')
-
-        locked = False
-        for line in config_file:
-            if line == 'deployment.security.askgrantdialog.notinca=false':
-                locked = True
         config.close()
         return locked
-
-
 
     def publisher_revocation_enabled(self):
         """Check SV-43604r1_rule: The dialog to enable users to 
@@ -133,14 +147,30 @@ class JREAuditor:
 
         Finding ID: V-32830     
         """
-        pass
+        config = open(self.properties_path, 'r')
+
+        disabled = False
+        for line in config_file:
+            if 'deployment.security.validation.crl=false' in line:
+                locked = True
+        config.close()
+        return locked
+
     def publisher_revocation_locked(self):
         """Check SV-43617r1_rule: The option to enable users to check 
         publisher certificates for revocation must be locked.
         
         Finding ID: V-32831     
-        """        
-        pass
+        """
+        config = open(self.properties_path, 'r')
+
+        disabled = False
+        for line in config_file:
+            if 'deployment.security.validation.crl.locked' in line:
+                locked = True
+        config.close()
+        return locked
+        
 
 
     def certificate_validation_enabled(self):
@@ -149,7 +179,15 @@ class JREAuditor:
         
         Finding ID: V-32832   
         """
-        pass
+        config = open(self.properties_path, 'r')
+
+        disabled = False
+        for line in config_file:
+            if 'deployment.security.validation.ocsp=false' in line:
+                locked = True
+        config.close()
+        return locked
+        
 
     def certificate_validation_locked(self):
         """Check SV-43619r1_rule: The option to enable online 
@@ -164,7 +202,7 @@ class JREAuditor:
             if line == 'deployment.security.validation.ocsp.locked':
                 locked = True
         config.close()
-
+        return locked
 
     def config_keys_set(self):
         """Check SV-43649r1_rule: The configuration file must contain
@@ -172,7 +210,18 @@ class JREAuditor:
         
         Finding ID: V-32842    
         """
-        pass
+        config = open(self.properties_path, 'r')
+
+        properties_set = False
+        deployment_set = False
+        for line in config_file:
+            if 'deployment.system.config=' in line: #This should end with properties filename
+                properties_set = True
+            if 'deployment.system.config.mandatory=false' in line:
+                deployment_set = True
+        config.close()
+        return properties_set and deployment_set
+        
 
 if __name__ == "__main__":
     auditor = JREAuditor()
